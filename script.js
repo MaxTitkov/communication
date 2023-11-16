dfacdf9ac9d0898845be9dce4efa5ef486122780
Maxim <breddowen@gmail.com>
2023-11-16 22:42:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 // Get all the section elements and menu items
 
 const menuItems = document.querySelectorAll('#menuElem');
+const menuItemsArray = Array.from(menuItems);
 const sections = document.querySelectorAll('#scrollItem');
-
+const sectionsArray = Array.from(sections);
 
 menuItems.forEach((menuItem) => {
 
@@ -17,12 +18,11 @@ menuItems.forEach((menuItem) => {
 
 })
 
-
-
 // Add scroll event listener
 window.addEventListener('scroll', () => {
     // Get the current scroll position
     const scrollPosition = window.scrollY;
+
   
     // Determine the active section
     let activeSection = sections[0];
@@ -30,22 +30,37 @@ window.addEventListener('scroll', () => {
       const sectionTop = section.offsetTop;
       if (scrollPosition >= sectionTop) {
         activeSection = section;
+        // const indexOfActiveSection = sectionsArray.indexOf(activeSection);
       }
     });
   
     // Highlight the corresponding menu item
     menuItems.forEach((menuItem) => {
       // Remove the active class from all menu items
-      menuItem.classList.remove('active');
+    //   menuItem.classList.remove('is-active');
       // Add the active class to the menu item corresponding to the active section
     //   if (menuItem.getAttribute('href') === `#${activeSection.id}`) {
     //     menuItem.classList.add('active');
     //   }
+
+    menuItemPosition = menuItem.getBoundingClientRect().top;
+
     if(menuItem.innerText.toLowerCase() === activeSection.innerText.toLowerCase()){
         menuItem.classList.add('is-active');
-    }else{
-        menuItem.classList.remove('is-active');
+        const activeMenuItemIndex = menuItemsArray.indexOf(menuItem);
+
+        const indexOfInactive = -(menuItemsArray.length - activeMenuItemIndex)+1
+        menuItemsArray.slice(indexOfInactive).forEach((item) => {
+            item.classList.remove('is-active');
+        })
+
+        if(activeMenuItemIndex === menuItemsArray.length - 3){
+            console.log(menuItemsArray[activeMenuItemIndex+1])
+            menuItemsArray[activeMenuItemIndex+1].classList.add('is-active');
+        }
+
     }
+
     });
   });
 
